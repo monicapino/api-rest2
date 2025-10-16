8d0f13b2144104e68d91aeed5ae993f09eecfcc2
monicapino <monica_pinno@yahoo.com.br>
2025-10-16 15:58:10 -0300

    feat: adiciona middleware, tipagem personalizada, e atualizar server.ts

---
+++ src/middlewares/myMiddleware.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from 'express';
+
+export function myMiddleware(
+    request: Request, 
+    response: Response, 
+    next: NextFunction 
+) {
+    request.user_id = "123456"
+
+    console.log('passou pelo Middleware, executed ')
+
+    return next()
+}
+
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,31 @@
 import express from 'express'
+import { myMiddleware } from './middlewares/myMiddleware'
+
+const PORT = 3333
 
 const app = express()
-const port = 3333
+app.use(express.json())
 
-app.get('/', (request, response) => {
-    const { id } = request.query
-    response.send('Hello World!')
+//app.use(myMiddleware)
+
+app.get("/products", myMiddleware, (request, response) => {
+    const { page, limit } = request.query
+
+    response.send(`Página ${page} - Limite ${limit}`)
 })
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`)
+
+app.post("/products", myMiddleware, (request, response) => {
+    const { name, price } = request.body 
+
+    response.status(201).json({ name, price, user_id: request.user_id })
 })
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`)
+})
+
+
+
+
+
